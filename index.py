dd544cd182754d3435c880ca71a001e989090371
yunpeng ma <mayunpeng@pjlab.org.cn>
2023-08-04 16:08:34 +0800

    a

---
+++ index.py
@@ -0,0 +1,26 @@
+'''
+import gradio as gr
+
+def greet(name):
+    return "Hello " + name + "!!"
+    
+iface = gr.Interface(fn=greet, inputs="text", outputs="text")
+iface.launch()
+'''
+
+import gradio as gr
+
+with gr.Blocks() as demo:
+    with gr.Row():
+        text1 = gr.Textbox(label="t1")
+        slider2 = gr.Textbox(label="s2")
+        drop3 = gr.Dropdown(["a", "b", "c"], label="d3")
+    with gr.Row():
+        with gr.Column(scale=1, min_width=1600):
+            text1 = gr.Textbox(label="prompt 1")
+            text2 = gr.Textbox(label="prompt 2")
+            inbtw = gr.Button("Between")
+            text4 = gr.Textbox(label="prompt 1")
+            text5 = gr.Textbox(label="prompt 2")
+
+demo.launch()

f88decedebea51665c884364950770e33b92e6ce
yunpeng ma <mayunpeng@pjlab.org.cn>
2023-08-10 13:31:38 +0800

    m

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import gradio as gr
+import time
 
 def greet(name):
     return "Hello " + name + "!!"
     
 iface = gr.Interface(fn=greet, inputs="text", outputs="text")
+time.sleep(60)
 iface.launch()
 
